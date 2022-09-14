92f9cefaffce79aaf6c5bc6f8d0b566a1437e892
goldflag <billyang@umich.edu>
2022-09-14 01:18:56 -0700

    Add comment

--- main.go
+++ main.go
@@ -15,6 +15,31 @@ import (
 func main() {
 	godotenv.Load()
 	r := mux.NewRouter()
+
+	/* Returns player tank data in compressed form to save bandwidth
+	 *
+	 * returns each tank data in an array of this order:
+	 * [
+	 *	  Tank_id,
+	 *	  Battles,
+	 *	  Damage_dealt,
+	 *	  Damage_received,
+	 *	  Frags,
+	 *	  Survived_battles,
+	 *	  Wins,
+	 *	  Losses,
+	 *	  Draws,
+	 *	  Capture_points,
+	 *	  Dropped_capture_points,
+	 *	  Xp,
+	 *	  Spotted,
+	 *	  Tanking_facto,
+	 *	  Avg_damage_blocked,
+	 *	  Shots,
+	 *	  Hits,
+	 *	  Piercings
+	 * ]
+	 */
 	r.HandleFunc("/fetchPlayer/{server}/{id}", endpoints.FetchPlayer)
 
 	http.ListenAndServe(fmt.Sprint(":", os.Getenv("PORT")), r)
